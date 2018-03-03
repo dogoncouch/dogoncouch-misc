b64e01bd1d3a0631c580c849574b7006caacc434
dogoncouch <dpersonsdev@gmail.com>
2018-03-03 17:26:35 -0500

    Update trials.py

--- trials.py
+++ trials.py
@@ -149,7 +149,7 @@ class TrialsCore:
 
         # Fix double-escaped newlines (by cheating):
         #os.system("sed -i 's/\\\\\\\\/\\\\/' " + self.args.out)
-        os.system("perl -i -pe 's/\\\\/\\/' " + self.args.out
+        os.system("perl -i -pe 's/\\\\/\\/' " + self.args.out)
 
 
 
